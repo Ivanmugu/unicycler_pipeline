9f558eab0cb7b863045e8628290f74c1ef05863c
Ivan Munoz-Gutierrez <ivan.munoz.gutierrez@gmail.com>
2021-03-19 17:48:57 -0700

    Added extra calling of fasta_extractor to make a copy of fasta files

--- unicycler_pipeline.sh
+++ unicycler_pipeline.sh
@@ -1,136 +1,198 @@
 #!/bin/bash
 #
-# Created on Sunday Feb 7, 2021
-# @author: ivanmugu
+# -----------------------------------------------------------------------------
 #
+# File name:          unicycler_pipeline.sh
+# Author:             Ivan Munoz Gutierrez
+# Date created:       02/07/2021
+# Date last modified: 02/19/2021
+# Description:        Process Illumina and ONP reads using Unicycler. This
+#                     script needs log_parser.py and fasta_extractor.py.
+#                     For more details refer to the HELP heredoc.
 #
-# Accessing folders and subfolders to retrieve information from files that
-# contain Illumina and ONP reads to run unicycler.py. After running unicycler,
-# the script will run log_parser.py and fasta_extractor.py. The log_parser.py
-# program will get important information from the tables inside unicycler.log 
-# file. The fasta_extractor.py will extract the individual fasta sequences
-# generated inside assembly.fasta.
-#
-# TODO: to finish including log_parser and fasta_extractor in the code
-#
+# -----------------------------------------------------------------------------
 
-# Function to print usage
-# Does not get arguments
-usage () {
-  echo "usage: $0 [-h HELP] -i input_folder -o output_folder"
+# USAGE
+readonly USAGE="Usage: $0 [-h HELP] -i input_folder -o output_folder"
+
+# HELP heredoc
+read -r -d '' HELP << EOM
+Pipeline to assemble genomes using Unicycler.
+
+Mandatory aguments:
+-i input_folder        provide path to input folder
+-o output_folder       provide path to output folder
+
+Optional arguments:
+-h                     show this help and exit
+
+To run this program you need a main folder. The input folder must contain
+subfolders with the Illumina and ONP reads that will be used for the assembly.
+For example, you can have the following tree folder structure:
+
+~/Documents/input/
+              SW0001/
+                  illumina_reads_1.fastq
+                  illumina_reads_2.fastq
+                  ONP_reads_L1000.fastq
+              SW0002/
+                  illumina_reads_1.fastq
+                  illumina_reads_2.fastq
+                  ONP_reads_L1000.fastq
+
+The script will first analyze the data in folder SW0001. The resulting files
+will be put in the output folder inside a new created folder that will be
+named SW0001. Then, the script will continue the same process with the SW0002
+folder present in the input folder. When the assembly is done, the
+unicycler.log files will be parsed to extract all the relevant information,
+generating two csv files. The csv files will be named molecules_summary and
+assemblies_summary, and will be located in the output folder. Additionally, the
+assembly.fasta fasta files will be parsed to extract the fasta sequences into
+independent files. The new created fasta files will be named according to the
+folder that contains the assembly.fasta file used for the extraction. The new
+fasta files names will contain the length and topology of the molecule.
+
+Usage example
+-------------
+
+$ ./unicycler_pipeline.sh -i ~/Documents/input -o ~/Documents/output
+
+A tree example of a hypothetical results using the input folder described
+before could be the one shown below. To handle future manipulation of the
+extracted fasta files, a new folder named assemblies inside output will also
+contained the extracted fasta files.
+
+~/Documents/output/
+              assemblies_summary.csv
+              molecules_summary.csv
+              SW0001/
+                  assembly.fasta
+                  unicycler.log
+                  SW0001_4000000_circular.fasta
+                  SW0001_100000_circular.fasta
+                  SW0001_50000_circular.fasta
+              SW0002/
+                  assembly.fasta
+                  unicycler.log
+                  SW0002_5000000_linear.fasta
+                  SW0002_200000_circular.fasta
+              assemblies/
+                  SW0001_4000000_circular.fasta
+                  SW0001_100000_circular.fasta
+                  SW0001_50000_circular.fasta
+                  SW0002_5000000_linear.fasta
+                  SW0002_200000_circular.fasta
+
+Notes
+-----
+Information about the programs used in unicycler_pipeline can be found in:
+  i)   unicycler: https://github.com/rrwick/Unicycler
+  ii)  log_parser: https://github.com/Ivanmugu/log_parser
+  iii) fasta_extractor: https://github.com/Ivanmugu/fasta_extractor
+EOM
+
+
+# -----------------------------------------------------------------------------
+# Print help
+# Globals:
+#   USAGE
+#   HELP
+# Outputs:
+#   Write help to stdout
+# -----------------------------------------------------------------------------
+function print_help() {
+  echo $USAGE
+  echo ""
+  echo "$HELP"
 }
 
-# Function to print help
-# Does not get arguments
-printHelp () {
-  echo
-  usage
-  echo
-  printf "Pipeline to assemble genomes\n\n"
-  printf "mandatory aguments:\n"
-  printf -- "-i input_folder        provide path to input folder\n"
-  printf -- "-o output_folder       provide path to output folder\n\n"
-  printf "optional arguments:\n"
-  printf -- "-h                     show this help and exit\n\n"
-  printf "To run this program you need a main folder. The input folder must\n"
-  printf "contain subfolders with the Illumina and ONP reads that will be used\n"
-  printf "for the assembly. For examle, you can have the following folders:\n"
-  printf "input/\n"
-  printf "    SW0001/\n"
-  printf "        illumina_reads_1.fastq\n"
-  printf "        illumina_reads_2.fastq\n"
-  printf "        ONP_reads_L1000.fastq\n"
-  printf "    SW0002/\n"
-  printf "        illumina_reads_1.fastq\n"
-  printf "        illumina_reads_2.fastq\n"
-  printf "        ONP_reads_L1000.fastq\n\n"
-  printf "The script will first analyze the data in folder SW0001 and put the\n"
-  printf "result files in the output folder in a folder that will be named SW001.\n"
-  printf "Then, the script will continue the same process with the folder SW0002\n"
-  printf "present in the input folder. When the assembly is done, the unicycler.log\n"
-  printf "files will be parsed to extract all the relevant information, generating\n"
-  printf "two csv files. The csv files will be named molecules_summary and\n"
-  printf "assemblies_summary, and will be located in the output folder.\n\n"
-  printf "Usage example:\n"
-  printf "pepito$ ./unicycler_pipeline.sh -i ~/Documents/input -o ~/Documents/output\n\n"
-}
 
-# Function to run unicycler
-# Arguments
-# --------
-# input [$1] : path to input folder
-# output [$2] : path to output folder
-runUnicycler () {
+# -----------------------------------------------------------------------------
+# Run unicycler program
+# Arguments:
+#   input [$1] : path to input folder
+#   output [$2] : path to output folder
+# Outputs:
+#   Files resulting from the assembly as assembly.fasta and unicycler.log are
+#   saved in the ouput folder.
+# -----------------------------------------------------------------------------
+function run_unicycler() {
   local input=$1
   local output=$2
-  # Accessing folders from input 
+
+  # Accessing folders from input. 
   for folder in $(ls ./$input)
   do
-      # Accessing subfolders from input
+      # Accessing subfolders from input.
       for file in $(ls ./$input/$folder)
       do
-          # Getting the names of arguments for unicycler
-          # extention is used to identify the type of read
+          # Getting the names of arguments for unicycler. The variable
+          # extention is used to identify the type of read.
           extention=${file##*-}
-          # Getting forward Illumina reads (1 indicates forward)
+          # Getting forward Illumina reads (1 indicates forward).
           if [[ "1.fastq" = $extention || "1.fastq.gz" = $extention ]]
           then
               forward=$file
-          # Getting reverse Illumina reads (2 indicates reverse)
+          # Getting reverse Illumina reads (2 indicates reverse).
           elif [[ "2.fastq" = $extention || "2.fastq.gz" = $extention ]]
           then
               reverse=$file
-          # Getting long ONP reads (L1000 indicates ONP)
+          # Getting long ONP reads (L1000 indicates ONP).
           elif [[ "L1000.fastq" = $extention || "L1000.fastq.gz" = $extention ]]
           then
               long=$file
-          # if file is not Illumina or ONP read ignore
+          # If file is not Illumina or ONP read ignore.
           else
             continue
           fi
       done
-      # Creating directory to save output of unicycler. The name of the
-      # directory in the output folder will be the same as in the input folder
+      # Creating directory to save the output of unicycler. The name of the
+      # directory in the output folder will be the same as in the input folder.
       mkdir ./$output/$folder
-      # Running unicycler
+      # Running unicycler.
       echo "Running unicycler with files from folder: $folder"
       unicycler -1 ./$input/$folder/$forward -2 ./$input/$folder/$reverse \
         -l ./$input/$folder/$long -t 32 --mode bold -o ./$output/$folder
   done
 }
 
-# Cheking provided arguments and getting inputFolder and outputFolder variables
-# Arguments
-# --------
-# $@ : All arguments provided in the command line
-parseArguments () {
+
+# -----------------------------------------------------------------------------
+# Parse command line arguments.
+# Globals:
+#   USAGE
+# Arguments:
+#   $@ : all arguments provided in the command line.
+# -----------------------------------------------------------------------------
+function parse_arguments() {
   local option
   local inputFlag=false
   local outputFlag=false
-  # Arguments to parse are -h -i and -o
+
+  # Arguments to parse are -h -i and -o.
   while getopts ":hi:o:" option
   do
     case $option in
-      # Priting help
-      h) printHelp; exit 1;;
-      # Getting path to input folder
+      # Priting help.
+      h) print_help; exit 1;;
+      # Getting path to input folder.
       i) inputFlag=true; inputFolder=$OPTARG;;
-      # Getting path to output folder
+      # Getting path to output folder.
       o) outputFlag=true; outputFolder=$OPTARG;;
-      # Printing error when flag is missing argument
-      :) echo "Error: -${OPTARG} requires an argument."; exit 1 ;;
-      # If unknown option exit
-      *) echo "Error: you provided and unkown argument."; usage; exit 1;;
+      # Printing error when flag is missing an argument.
+      :) echo "Error: -${OPTARG} requires an argument."; exit 1;;
+      # If unknown option, exit.
+      *) echo "Error: you provided and unkown argument."; echo $USAGE; exit 1;;
     esac
   done
-  # Checking if all mandatory flags were provided
+  # Checking if all mandatory flags were provided.
   if [[ $inputFlag = false || $outputFlag = false ]]
   then
     echo "Error: you forgot to provide input or output."
-    usage
+    echo $USAGE
     exit 1
   fi
-  # Checking if inputFolder and outputFolder exist
+  # Checking if inputFolder and outputFolder exist.
   if [[ ! -d $inputFolder ]] 
   then
     echo "Error: input_folder $inputFolder does not exist"
@@ -143,20 +205,20 @@ parseArguments () {
     echo "input_folder and output_folder exist"
   fi
  
-  # Checking if paths to input and output folders have correct format
-  #
-  # Getting lengh of input and output path
+  # ----------------------------------------------------------------------
+  # Checking if paths to input and output folders have the correct format.
+  # ----------------------------------------------------------------------
+  # Getting lengh of input and output path.
   lenInput=${#inputFolder}
   lenOutput=${#outputFolder}
-  # Getting the last character if input and output path
+  # Getting the last character if input and output path.
   lastCharInput=${inputFolder:lenInput - 1:1}
   lastCharOutput=${outputFolder:lenOutput - 1:1}
-  # Checking format of input folder
+  # If last character in path is not "/" add the "/" to the path.
   if [[ ! $lastCharInput = "/" ]]
   then
     inputFolder="${inputFolder}/"
   fi
-  # Checking format of output folder
   if [[ ! $lastCharOutput = "/" ]]
   then
     outputFolder="${outputFolder}/"
@@ -165,37 +227,53 @@ parseArguments () {
   echo "Path output folder: $outputFolder"
 }
 
-# main function
-# Arguments
-# ---------
-# $@ : all the arguments provided in the command line
-main () {
-  # Getting user input
-  parseArguments $@
 
-  # Running unicycler
+# -----------------------------------------------------------------------------
+# Main.
+# Arguments:
+#   $@ : all arguments provided in the command line.
+# -----------------------------------------------------------------------------
+function main() {
+  # -------------------
+  # Getting user input.
+  # -------------------
+  parse_arguments $@
+
+  # ------------------
+  # Running unicycler.
+  # ------------------
   printf "\nRunning unicycler\n"
-  runUnicycler $inputFolder $outputFolder
+  run_unicycler $inputFolder $outputFolder
 
-  # Extracting tables from unicycler.log file
-  # log_parser.py will loop over the folders inside outputFolder to parse the 
-  # generated unicycler.log file in each assembly. Therefore, in this case,
-  # the path the input folder for log_parser must be the path to output folder
-  # of the runUnicycler function. For more information print the help of
-  # log_parser.py
+  # ---------------------------------------------------------------
+  # Extracting tables from unicycler.log files using log_parser.py.
+  # Flags:
+  #   -i : path to input folder.
+  #   -o : path to output folder.
+  # ---------------------------------------------------------------
   printf "\nExtracting tables from unicycler.log\n"
   python3 log_parser.py -i $outputFolder -o $outputFolder
 
-  # Extracting fasta sequences from assembly.fasta
-  # -n means name of input fasta file and -d means path do input directory.
-  # Because assembly.fasta is created in the output directory after running
-  # unicycler, the input directory (-d) is going to the output directory of
-  # unicycler.
+  # ------------------------------------------------------------------------
+  # Extracting fasta sequences from assembly.fasta using fasta_extractor.py.
+  # Flags:
+  #   -n : name of input fasta file.
+  #   -i : path to input folder.
+  #   -o : path to output folder. This flag is optional. If it is not provided
+  #        the extracted fasta files will be saved in the same folder that
+  #        contains the assembly.fasta used for the extraction.
+  # ------------------------------------------------------------------------
   printf "\nExtracting fasta sequences from assembly.fasta\n"
-  python3 fasta_extractor.py -n assembly.fasta -d $outputFolder
+  python3 fasta_extractor.py -n assembly.fasta -i $outputFolder
+
+  # Make a folder to save a copy of all extracted fasta files
+  mkdir $outputFolder/assemblies
+  python3 fasta_extractor.py -n assembly.fasta -i $outputFolder \
+  -o $outputFolder/assemblies 
 
   echo "unicycler_pipeline is done!"
 }
 
-# Running the script
-main $@
+
+# Let's have fun
+main "$@"
